c97c7b9e1cd71f97f2ee952e66493952d53bb59a
DeadlyMC <modding.deadlymc@gmail.com>
2020-06-25 20:19:34 +0530

    Added carpet rule to remove connecting timeout
     Co-authored-by: X-com

--- carpetmodSrc/carpet/CarpetSettings.java
+++ carpetmodSrc/carpet/CarpetSettings.java
@@ -723,6 +723,9 @@ public class CarpetSettings
     
     @Rule(desc = "Turns crafting tables into automated crafting tables with inventorys.", category = FEATURE, extra = "WARNING! If the rule is turned off after use, any inventory content in crafting tables will permanently become lost after chunks are reloaded.")
     public static boolean autocrafter;
+    
+    @Rule(desc = "Removes the timeout caused by delays attempting to login.", category = FIX)
+    public static boolean removeConnectionTimeout;
 
 
     // ===== API ===== //
